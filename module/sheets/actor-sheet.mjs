4b234b1b9a4026c8d4fe99d3090f4b0cb0def08a
Tenyryas <tenyryas@gmail.com>
2022-05-03 21:53:49 +0200

    Skills rolls first draft

--- module/sheets/actor-sheet.mjs
+++ module/sheets/actor-sheet.mjs
@@ -180,6 +180,7 @@ export class CapharnaumActorSheet extends ActorSheet {
 
     // Rollable attributes.
     html.find('.rollableAttribute').click(this._onRollAttribute.bind(this));
+    html.find('.rollableSkill').click(this._onRollSkill.bind(this));
 
     html.find('.resetHeroism').click(this._onHeroClick.bind(this));
 
@@ -292,7 +293,7 @@ export class CapharnaumActorSheet extends ActorSheet {
 
   }
 
-  _onRollAttribute(event) {
+  async _onRollAttribute(event) {
 
     // Use a safe clone of the actor data for further operations.
     const actorData = this.actor.data.toObject(false);
@@ -300,41 +301,107 @@ export class CapharnaumActorSheet extends ActorSheet {
     event.preventDefault();
     const element = event.currentTarget;
     const dataset = element.dataset;
-    let attribute;
+    const attribute = dataset.label;
 
-    // Grabs the right attribute from the label
-    switch (dataset.label) {
-      case game.i18n.localize(CONFIG.CAPHARNAUM.attributes["str"]):
-        attribute = "str";
-        break;
-
-      case game.i18n.localize(CONFIG.CAPHARNAUM.attributes["dex"]):
-        attribute = "dex";
-        break;
-
-      case game.i18n.localize(CONFIG.CAPHARNAUM.attributes["con"]):
-        attribute = "con";
-        break;
-
-      case game.i18n.localize(CONFIG.CAPHARNAUM.attributes["int"]):
-        attribute = "int";
-        break;
-
-      case game.i18n.localize(CONFIG.CAPHARNAUM.attributes["cha"]):
-        attribute = "cha";
-        break;
-
-      default:
-        break;
-    }
 
     const dice = actorData.data.attributes[attribute].value + actorData.data.virtues.heroism.max;
     const dragon = actorData.data.dragon_dice;
     let total = dice - dragon;
     let roll;
 
-    let formula;
+    let formula = this.formulaConcat(total, dragon);
 
+
+    roll = new Roll(formula);
+
+    // Prepare message
+
+    roll.toMessage({
+      speaker: ChatMessage.getSpeaker({ actor: this.actor }),
+      flavor: `${game.i18n.localize(CONFIG.CAPHARNAUM.attributes[attribute])} | ${game.i18n.localize("CAPHARNAUM.AttributeRoll")}`,
+      rollMode: game.settings.get('core', 'rollMode'),
+    });
+
+    // Sort dice by result
+
+    roll.dice[0].results.sort(function (a, b) { return a.result - b.result });
+
+    return roll;
+  }
+
+  async _onRollSkill(event) {
+
+    const element = event.currentTarget;
+    const dataset = element.dataset;
+    const figure = dataset.figure;
+    const skill = dataset.skill;
+    let attribute;
+    const rendered_dialog = await renderTemplate("systems/capharnaum/templates/dialogs/rollDialog.html");
+
+    let d = new Dialog({
+      title: game.i18n.localize("CAPHARNAUM.SkillRoll"),
+      content: rendered_dialog,
+      buttons: {
+        roll: {
+          icon: '<i class="fas fa-dice"></i>',
+          label: game.i18n.localize("CAPHARNAUM.DialogButtonRoll"),
+          callback: html => {
+            attribute = html.find('.attrSelect').val();
+            this.rollSkill(attribute, figure, skill);
+          }
+        },
+        cancel: {
+          icon: '<i class="fas fa-times"></i>',
+          label: game.i18n.localize("CAPHARNAUM.DialogButtonCancel"),
+        }
+      },
+      default: "cancel"
+    });
+    d.render(true);
+
+  }
+
+  rollSkill(attr, figure, skill) {
+
+    const actorData = this.actor.data.toObject(false);
+    const attrValue = actorData.data.attributes[attr].value;
+    const skillValue = actorData.data.skills[figure][skill].value;
+
+    const dice = attrValue + skillValue;
+    const dragon = actorData.data.dragon_dice;
+    let flavorText = "";
+
+    let total = dice - dragon;
+    let roll;
+
+    let formula = this.formulaConcat(total, dragon);
+
+
+    roll = new Roll(formula);
+
+    // Prepare message
+    // if the character has rolled skill at zero, it's an unskilled test.
+    if (skillValue === 0) {
+      flavorText = `${game.i18n.localize(CONFIG.CAPHARNAUM.attributes[attr])} + ${game.i18n.localize(CONFIG.CAPHARNAUM.skills[skill])} | ${game.i18n.localize("CAPHARNAUM.UnskilledRoll")}`;
+    } else {
+      flavorText = `${game.i18n.localize(CONFIG.CAPHARNAUM.attributes[attr])} + ${game.i18n.localize(CONFIG.CAPHARNAUM.skills[skill])} | ${game.i18n.localize("CAPHARNAUM.SkillRoll")}`;
+    }
+
+    roll.toMessage({
+      speaker: ChatMessage.getSpeaker({ actor: this.actor }),
+      flavor: flavorText,
+      rollMode: game.settings.get('core', 'rollMode'),
+    });
+
+    roll.dice[0].results.sort(function (a, b) { return a.result - b.result });
+
+    return roll;
+
+  }
+
+  formulaConcat(total, dragon) {
+
+    let formula;
 
     // Check if total of dice is less or equal to 0.
     // If it is, roll as much dragon dice as possible (const dice)
@@ -357,8 +424,6 @@ export class CapharnaumActorSheet extends ActorSheet {
 
       }
 
-      roll = new Roll(formula);
-
     } else {
 
       formula = total + "d6";
@@ -376,26 +441,9 @@ export class CapharnaumActorSheet extends ActorSheet {
         formula = formula.concat(" + ", dragonDieString);
 
       }
-
-      roll = new Roll(formula);
     }
 
-    // Prepare message
-
-    console.log(
-    roll.toMessage({
-      speaker: ChatMessage.getSpeaker({ actor: this.actor }),
-      flavor: `${dataset.label} ${game.i18n.localize("CAPHARNAUM.AttributeTest")}`,
-      rollMode: game.settings.get('core', 'rollMode'),
-    })
-    );
-
-    // Sort dice by result
-
-    roll.dice[0].results.sort(function (a, b) { return a.result - b.result });
-
-    return roll;
+    return formula;
   }
-
 
 }
